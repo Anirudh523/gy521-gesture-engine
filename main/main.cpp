17f79861d35be8d3becc8d5dc0085803c904373d
Anirudh523 <aniaar523@gmail.com>
2026-07-03 23:41:37 -0700

    Now the monitor is showing thecorrect values for shakes and all that stuff.

--- main/main.cpp
+++ main/main.cpp
@@ -3,25 +3,48 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 
-static const char* TAG = "main";
+QueueHandle_t frameQueue;
+QueueHandle_t resultQueue;
+
+void sensorTask(void* pvParameters) {
+    IMUSensor imu;
+    while (true) {
+        auto frame = imu.read();
+        xQueueSend(frameQueue, &frame, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+}
+
+void pipelineTask(void* pvParameters) {
+    FeaturePipeline<50> pipeline;
+    IMUFrame frame;
+    while (true) {
+        xQueueReceive(frameQueue, &frame, portMAX_DELAY);
+        pipeline.push(frame);
+        if (pipeline.isReady()) {
+            auto features = pipeline.getFeatures();
+            xQueueSend(resultQueue, &features, portMAX_DELAY);
+            pipeline.reset();
+        }
+    }
+}
+
+void outputTask(void* pvParameters) {
+    FeatureVector features;
+    while (true) {
+        xQueueReceive(resultQueue, &features, portMAX_DELAY);
+        ESP_LOGI("output", "rms_ax=%.3f mean_ax=%.3f zcr_ax=%.3f",
+            features.rms[0], features.mean[0], features.zcr[0]);
+    }
+}
 
 extern "C" void app_main() {
+    frameQueue = xQueueCreate(10, sizeof(IMUFrame));
+    resultQueue = xQueueCreate(5, sizeof(FeatureVector));
 
-IMUSensor imu; 
-FeaturePipeline<50> pipeline;
-
-
-while (true) {
-
-auto frame = imu.read();
-pipeline.push(frame);
-
-if(pipeline.isReady()){
-	auto features = pipeline.getFeatures();
-	ESP_LOGI(TAG, "rms_ax=%.3f mean_ax=%.3f zcr_ax=%.3f", features.rms[0], features.mean[0], features.zcr[0]);
-	pipeline.reset();
-}
-vTaskDelay(pdMS_TO_TICKS(100));
-}
+    xTaskCreate(sensorTask, "sensor_task", 4096, NULL, 5, NULL);
+    xTaskCreate(pipelineTask, "pipeline_task", 4096, NULL, 4, NULL);
+    xTaskCreate(outputTask, "output_task", 4096, NULL, 3, NULL);
 }
